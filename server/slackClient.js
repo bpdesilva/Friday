d05b4e8f6dc5dde5885e9390fef7d7c5d2724ac6
Buwaneka De Silva <buwanekapasan@gmail.com>
2019-03-09 20:43:29 +0530

    Update

--- server/slackClient.js
+++ server/slackClient.js
@@ -1,7 +1,7 @@
 const { RTMClient } = require('@slack/client');
 
 // An access token (from your Slack app or custom integration - usually xoxb)
-const token = '179359101623.571762522980';
+const token = '';
 
 // The client is initialized and then started to get an active connection to the platform
 const rtm = new RTMClient(token);
