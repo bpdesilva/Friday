3825eee494a28f83b0b6e3275512d19d268cff15
Buwaneka De Silva <buwanekapasan@gmail.com>
2019-03-10 01:07:12 +0530

    Wit.ai Integration

--- bin/run.js
+++ bin/run.js
@@ -5,14 +5,17 @@ const service = require('../server/service');
 const http = require('http');
 const server = http.createServer(service);
 
+const witToken = '';
+const witClient = require('../server/witClient')(witToken);
+
 const slackToken = '';
 const slackLogLevel = 'verbose';
 
-const rtm = slackClient.init(slackToken, slackLogLevel);
+const rtm = slackClient.init(slackToken, slackLogLevel, witClient);
 rtm.start();
 
 slackClient.addAuthenticatedHandler(rtm, () => server.listen(3000));
 
 server.on('listening', function() {
-    console.log(`Friday is listening on ${server.address().port} in ${service.get('env')} mode.`);
+    console.log(`IRIS is listening on ${server.address().port} in ${service.get('env')} mode.`);
 });
--- server/slackClient.js
+++ server/slackClient.js
@@ -1,9 +1,48 @@
 'use strict';
 
-const { RTMClient } = require('@slack/client');
+const {RTMClient} = require('@slack/client').RtmClient;
+let rtm = null;
+let nlp = null;
 
 function handleOnAuthenticated(rtmStartData) {
     console.log(`Logged in as ${rtmStartData.self.name} of team ${rtmStartData.team.name}, but not yet connected to a channel`);
+}
+
+function handleOnMessage(message) {
+
+    if (message.text.toLowerCase().includes('friday')) {
+        nlp.ask(message.text, (err, res) => {
+            if (err) {
+                console.log(err);
+                return;
+            }
+
+            try {
+                if (!res.intent || !res.intent[0] || !res.intent[0].value) {
+                    throw new Error("Could not extract intent.");
+                } 
+
+                const intent = require('./intents/' + res.intent[0].value + 'Intent');
+
+                intent.process(res, function(error, response) {
+                    if(error) {
+                        console.log(error.message);
+                        return;
+                    }
+
+                    return rtm.sendMessage(response, message.channel);
+                });
+
+            } catch (err) {
+                console.log(err);
+                console.log(res);
+                return rtm.sendMessage("Sorry, I don't know what you are talking about");
+            }
+
+        });
+    }
+
+
 }
 
 function addAuthenticatedHandler(rtm, handler) {
@@ -11,9 +50,11 @@ function addAuthenticatedHandler(rtm, handler) {
 }
 
 
-module.exports.init = function slackClient(token, logLevel) {
-    const rtm = new RTMClient(token, {logLevel: logLevel});
+module.exports.init = function slackClient(token, logLevel, nlpClient) {
+    rtm = RTMClient(token, { logLevel: logLevel });
+    nlp = nlpClient;
     addAuthenticatedHandler(rtm, handleOnAuthenticated);
+    rtm.on('message', handleOnMessage);
     return rtm;
 }
 
